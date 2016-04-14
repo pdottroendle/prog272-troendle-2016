d86c06e33637701b9c07875854c0a2cfdd8a9323
troendle at school <pdottroendle@gmail.com>
2016-04-13 18:02:47 -0700

    Apr13evening

---
+++ Week02-ObjectBasicsJasmine/Source/MyObject.js
@@ -0,0 +1,26 @@
+var MyObject = {
+
+    getOne: function() {
+        'use strict';
+        return 1;
+    },
+
+    getTwo: function() {
+        'use strict';
+        return 2;
+    },
+
+    getString: function() {
+        'use strict';
+        return 'string';
+    },
+
+    one: 1,
+
+    two: 2,
+
+    String: 'string'
+
+};
+
+module.exports = MyObject;
