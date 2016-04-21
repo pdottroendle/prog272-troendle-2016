a46291084555d3e8469ee90bc869add040453ff7
troendle at school <pdottroendle@gmail.com>
2016-04-20 18:35:26 -0700

    Week03 JS 2

--- Week03-ExpressJQuery/public/javascripts/control.js
+++ Week03-ExpressJQuery/public/javascripts/control.js
@@ -7,6 +7,7 @@ var MyObject = (function() {
     function MyObject() {
         $('#sendString').click(showString);
         $('#getItems').click(showItems);
+        $('#getMarie').click(showMarie);
     }
 
     function showItems() {
@@ -15,14 +16,33 @@ var MyObject = (function() {
         $("#myList").append('<li>' + 'item03' + '</li>');
     }
 
+    function showMarie() {
+        /*var marie = {
+            "firstName": "Marie",
+            "lastName": "Curie",
+            "city": "Paris",
+            "country": "France"
+        }*/
+        $.getJSON("marie.json", function (marie) {
+            console.log(marie);
+
+            for (var property in marie) {
+              if (marie.hasOwnProperty(property)) {
+                $("#myList2").append('<li>' + marie[property] + '</li>');
+              }
+            }
+
+        });
+    }
+
     function showString() {
         $('#stringHolder').html('Send string was clicked');
     }
 
-
     MyObject.prototype.readyCalled = function() {
         $("#readyCalled").html("Ready was called and myObjected created");
     };
+
 
     return MyObject;
 }());
@@ -31,5 +51,4 @@ var MyObject = (function() {
 $(document).ready(function() {
     var myObject = new MyObject();
     myObject.readyCalled();
-    myObjects.showItems();
 });
