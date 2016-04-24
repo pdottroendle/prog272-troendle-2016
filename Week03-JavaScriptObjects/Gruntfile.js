5b0cdef8a60454101318888f27a0ceb5078c1482
Troendle <pdottroendle@gmail.com>
2016-04-24 07:35:29 -0700

    grunted JSobjects

---
+++ Week03-JavaScriptObjects/Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: '<json:package.json>',
+
+        jshint: {
+            files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+
+            options: {
+                ignores: [
+                    '**/node_modules/**',
+                    '**/Library/jas/**',
+                    '**/platforms/**',
+                    '**/plugins/**',
+                    '**/jquery-2.0.3.js',
+                    '**/requirejs-wrapper*.js',
+                    '**/requirejs-setup*.js'
+                ],
+                reporter: 'checkstyle',
+                reporterOutput: 'result.xml',
+                strict: true,
+                globals: {
+                    describe: true,
+                    afterEach: true,
+                    beforeEach: true,
+                    inject: true,
+                    it: true,
+                    jasmine: true,
+                    expect: true,
+                    module: true,
+                }
+            }
+        },
+
+        clean: {
+            work: {
+                src: ['**/node_modules/**', 'result.xml']
+            },
+
+            zip: {
+                src: []
+            }
+        },
+
+        jscs: {
+            src: ['**/*.js'],
+            options: {
+                config: '.jscsrc'
+            }
+        },
+
+        'jsbeautifier': {
+            files: ['**/*.js', '!**/node_modules/**', '!**/components/**',
+                '!**/platforms/**', '!**/plugins/**'
+            ],
+            options: {
+                'indentSize': 4
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.registerTask('beautify', ['jsbeautifier']);
+    grunt.registerTask('check', ['beautify', 'jscs', 'jshint']);
+};
--- Week03-JavaScriptObjects/work.js
+++ Week03-JavaScriptObjects/work.js
@@ -1,9 +1,62 @@
-var person = { 
-	"fname": "Suzie" , 
-	"lname": "Wong" 
+var myObject01 = {
+    firstName: 'Susi',
+    lastName: 'Smooth'
 };
 
-person.fname = "Peter";
-person.lname = "Troendle";
+var myObject02 = {
+    firstName: 'Susi',
+    lastName: 'Smooth',
+    fullName: function() {
+        'use strict';
+        return this.firstName + ' ' + this.lastName;
+    }
+};
 
-console.log(person);
+var calculator = {
+    operator01: 6,
+    operator02: 10,
+    addMe: function() {
+        return this.operator01 + this.operator02;
+    },
+    subMe: function() {
+        return this.operator01 - this.operator02;
+    },
+    mulMe: function() {
+        return this.operator01 * this.operator02;
+    },
+    divMe: function() {
+        return this.operator01 / this.operator02;
+    }
+};
+
+function layout(title) {
+    console.log('');
+    console.log('====================================');
+    console.log(title);
+    console.log('====================================');
+}
+
+function sublayout(title) {
+    console.log('................................');
+    console.log(title);
+}
+
+layout('Names');
+console.log(myObject01.firstName);
+console.log(myObject01.lastName);
+console.log(myObject02.fullName());
+
+layout('Calculator');
+console.log('operator01 =', calculator.operator01);
+console.log('operator02 =', calculator.operator02);
+
+sublayout('Adder');
+console.log(calculator.addMe());
+sublayout('Subtractor');
+console.log(calculator.subMe());
+sublayout('Multiplyier');
+console.log(calculator.mulMe());
+sublayout('Divider');
+console.log(calculator.divMe());
+
+layout('Thank You!');
