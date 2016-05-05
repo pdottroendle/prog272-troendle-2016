9d16242c61bf59743c668862c40f5829d2e1bb05
troendle at school <pdottroendle@gmail.com>
2016-05-04 17:34:14 -0700

    solar 272

---
+++ Week05-ExpressRoutesSolar/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) { 'use strict';
+  res.render('index', { title: 'Week05-ExpressRoutesSolar Troendle' });
+});
+
+router.get('/renewables', function(request, response) {
+  console.log('Renewables called');
+
+  fs.readFile('data/Renewable.json', 'utf8', function(err, data)
+  {
+    if (err) throw err;
+    console.log(data);
+    response.send({result: 'Success', renewables: data});
+  });
+
+});
+
+module.exports = router;
