661480e391ce9aa418c63723fd3d1abd4db9eea4
troendle at school <pdottroendle@gmail.com>
2016-05-11 19:28:32 -0700

    errors corrected

--- Week05-FunctionThis/public/javascripts/control.js
+++ Week05-FunctionThis/public/javascripts/control.js
@@ -2,19 +2,41 @@ function getNine() {
     return 9;
 }
 
-function getThisStrict() { 'use strict'
+function getThisStrict() {
+    'use strict';
     return this;
 }
 
-var getThisAnonymous = function(){
-  return this;
+function getThis() {
+    return this;
+}
+
+var getThisAnonymous = function () {
+    return this;
 };
 
 var myObject = {
-   getThis: function() {
-       return this;
-   }
+    getThis: function () {
+        'use strict';
+        return this;
+    }
 };
 
-$(document).ready(function() { 'use strict';
+var myFunction = {
+    getThis: function () {
+        'use strict';
+        return this;
+    }
+};
+
+function MyFunction() {
+    MyFunction.prototype.getThis = function () {
+        return this;
+    }
+}
+
+
+
+$(document).ready(function () {
+    'use strict';
 });
