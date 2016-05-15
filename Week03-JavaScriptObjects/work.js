93ea3f2fa328974dfaac946b6df14d7b4e9904af
Troendle <pdottroendle@gmail.com>
2016-05-14 19:09:42 -0700

    corrections from our instructor

--- Week03-JavaScriptObjects/work.js
+++ Week03-JavaScriptObjects/work.js
@@ -1,3 +1,79 @@
+function divider(title) {
+	console.log("====================================");
+	console.log(title);
+	console.log("====================================");
+}
+
+var personTest01 = { 'name': 'Suzie' };
+var personTest02 = {};
+personTest02.name = 'Suzie';
+
+
+divider('Person Test');
+console.log('PersonTest01.name:', personTest01.name);
+console.log('PersonTest02.name:', personTest02.name);
+
+var person = {
+    firstName: 'George',
+    lastName: 'Washington',
+
+    fullName: function() {
+        'use strict';
+        return this.firstName + ' ' + this.lastName;
+    }
+};
+
+person.sayGoodbye = function() {
+	'use strict';
+	console.log('Goodbye');
+}
+
+// console.log(person);
+divider('Person');
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName());
+//person.firstName = 'cadmus';
+//console.log(person.fullName());
+
+/*****************************
+ * Calculator
+ ****************************/
+
+var calculator = {
+
+    operator01: 0,
+    operator02: 0,
+
+    add: function() {
+        'use strict';
+        return this.operator01 + this.operator02;
+    },
+
+    subtract: function() {
+        'use strict';
+        return this.operator01 - this.operator02;
+    }
+};
+
+calculator.operator01 = person.firstName.length;
+calculator.operator02 = person.lastName.length;
+
+calculator.multiply = function() {
+    'use strict';
+    return this.operator01 * this.operator02;
+};
+
+divider('Calculator');
+console.log('operator01 =', calculator.operator01);
+console.log('operator02 =', calculator.operator02);
+console.log('Add: ', calculator.add());
+console.log('Subtract:', calculator.subtract());
+console.log('Multiply:', calculator.multiply());
+
+person.sayGoodbye();
+
+/*    my version was, the above is the version from our instructor
 #!/usr/bin/env node
 
 var myObject01 = {
@@ -58,3 +134,6 @@ sublayout('Divider 7/8');
 console.log(calculator.divMe(7,8));
 
 layout('Thank You!');
+
+
+*/
