cbe90fcdae31e8457baac8586046ae310273b69c
troendle at school <pdottroendle@gmail.com>
2016-05-16 17:38:58 -0700

    df

--- Week07-ExpressRoutesSolar/public/javascripts/control.js
+++ Week07-ExpressRoutesSolar/public/javascripts/control.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     'use strict';
-    'use strict';
 
     $('#getRenewable').click(getRenewable);
+    $('#getRenewablebyIndex').click(getRenewableByIndex);
+    $('#getRenewablebyYear').click(getRenewableByYear);
+
 
     function getRenewable() {
         console.log('getRenewable called');
@@ -12,15 +14,92 @@ $(document).ready(function () {
             $('#debug').html(JSON.stringify(response, null, 4));
         })
             .done(function () {
-                console.log("second success");
+                console.log('second success');
             })
             .fail(function (a, b, c) {
                 console.log('Error', a, b, c);
                 $('#debug').html('Error occured: ', a.status);
             })
             .always(function () {
-                console.log("complete");
+                console.log('complete');
 
             });
     }
+
+    function getRenewableByIndex() {
+        console.log('getRenewableByIndex called');
+
+        var userInput = $('# renewableByIndex').val();
+
+
+            $.getJSON('/renewableByIndex/'+ userInput, function (response) {
+            console.log(response);
+
+            $('#debug').html(JSON.stringify(response, null, 4));
+        })
+            .done(function () {
+                console.log('second success');
+            })
+            .fail(function (a, b, c) {
+                console.log('Error', a, b, c);
+                $('#debug').html('Error occured: ', a.status);
+            })
+            .always(function () {
+                console.log('complete');
+
+            });
+    }
+
+    function getRenewableByYear() {
+        console.log('getRenewableByYear called');
+
+        var userInput = $('# renewableByYear').val();
+
+
+        $.getJSON('/renewableByYear/'+ userInput, function (response) {
+            console.log(response);
+
+            $('#debug').html(JSON.stringify(response, null, 4));
+        })
+            .done(function () {
+                console.log('second success');
+            })
+            .fail(function (a, b, c) {
+                console.log('Error', a, b, c);
+                $('#debug').html('Error occured: ', a.status);
+            })
+            .always(function () {
+                console.log('complete');
+
+            });
+    }
+
 });
+
+
+define(['jquery'], function($) {
+        //Do setup work here
+
+        function showBar() {
+            //console.log('Show Bar Clicks called now');
+            $('#display2').html('bar');
+        }
+
+        var control = {
+            color: "black",
+            size: "unisize",
+            setup: function() {
+                $(document).on('click', '#showClick', showBar);
+                $('#display2').html(control.color + ' - ' + control.size);
+            },
+            init: function() {
+                //console.log(this.color);
+                that = this;
+                $('#aboutButton').click(about.init);
+                $('#workButton').click(work.init);
+                $('#elf-view').load('/main', this.setup);
+            }
+        };
+
+        return control;
+    });
