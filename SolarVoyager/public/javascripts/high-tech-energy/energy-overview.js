d1ae68212571359b2b55fee7cef801517fa24777
Troendle <pdottroendle@gmail.com>
2016-06-03 08:29:52 -0700

    server/client side OK

--- SolarVoyager/public/javascripts/high-tech-energy/energy-overview.js
+++ SolarVoyager/public/javascripts/high-tech-energy/energy-overview.js
@@ -1,40 +1,90 @@
-/**
- * Created by bcuser on 5/28/16.
- */
-define(function () {
+define(['jquery'], function ($) {
     'use strict';
     var index = 0;
-
+    console.log('energy-types.js');
     function getRenewable() {
-        console.log('getRenewable called');
-        $.getJSON('/high-tech-energy', function (response) {
+        console.log('energy-types called');
+        $.getJSON('/high-tech-energy/', function (response) {
                 console.log(response);
+
+                ZZZZ.energyList = response.renewables; //  cc < ==== HERE
+                showRenewable(ZZZZ.energyList[index]); //  cc < ==== HERE
+
                 $('#debug').html(JSON.stringify(response, null, 4));
-            })
-            .done(function () {
-                console.log('second success');
             })
             .fail(function (a, b, c) {
                 console.log('Error', a, b, c);
                 $('#debug').html('Error occured: ', a.status);
+            })
+            .done(function () {
+                console.log('second success');
             })
             .always(function () {
                 console.log('complete');
             });
     }
 
-    var renewables = {
-        color: 'display of the energy data ',
-        size: 'big',
+    function getSimpleKeys(renewable) {
+        'use strict';
+        return {
+            year: renewable['MSN'],
+            solar: renewable['YYYYMM'],
+            geo: renewable['Value'],
+            biomass: renewable['Column_Order'],
+            wind: renewable['Description'],
+            liquid: renewable['Unit'],
+        }
+    }
+
+    function showRenewable(renewable) {
+        'use strict';
+        var renewablex = getSimpleKeys(renewable);  // the variable name is now distinguished by a separate name
+        $('#yearView').html(renewablex.year);    //.html is used instead of .val for the display
+        $('#solarView').html(renewablex.solar);
+        $('#geoView').html(renewablex.geo);
+        $('#biomassView').html(renewablex.biomass);
+        $('#windView').html(renewablex.wind);
+        $('#liquidView').html(renewablex.liquid);
+    }
+
+    function indexChange(test) {                 //   static form value is read and written back validated
+        if (test < 7009 && test >= 0) {
+            index = test;
+            $('#indexInput').val(index);         // the value is send back to the field
+            showRenewable(ZZZZ.energyList[index]);
+        }
+    }
+
+    var indexButtonChange = function (event) {  // the user  +  or  - event is added or substracted to the value
+        var test = event.data.value + index;
+        indexChange(test);
+    };
+
+    var buttonChange = function () {
+        var test = $('#indexInput').val();     // the default value or the user filled input is taken
+        indexChange(parseInt(test));
+    };
+
+    var ZZZZ = {                    // ppt < ==== needed a variable name that more distinctable than "energy"
+        color: 'display of the energy data',
+        size: 'client side index, see on the bottom of the list',
+        energyList: [],         //  cc < ==== HERE
+        getRenewable: getRenewable, //  cc < ==== HERE
         init: function () {
-            $('#elf-view').load('/renewables/renewables-page', function () {
-                $('#renewable').change(function () {
-                    getRenewable();
-                });
+            console.log(ZZZZ.color);
+            $('#elf-view').load('/high-tech-energy/energy-overview-page', function () {
+                $('#display').html(ZZZZ.color);
+                $('#display2').html(ZZZZ.size);
+                $('#plusButton').click({value: +1}, indexButtonChange);  // note the list has a descending order
+                $('#minusButton').click({value: -1}, indexButtonChange);
+                $('#indexInput').change(buttonChange);
+                //$('#XXXX').change(function () {     // ppt < ==  renamed for clarity purpose "energy"
+                //    getRenewable();
+                //});                  // ppt < ====== seemed not beeing used
                 getRenewable();
             });
         }
 
     };
-    return renewables;
+    return ZZZZ;
 });
--- SolarVoyager/public/javascripts/high-tech-energy/energy-types.js
+++ SolarVoyager/public/javascripts/high-tech-energy/energy-types.js
@@ -1,62 +1,93 @@
-/**
- * Created by bcuser on 5/22/16.
- */
-$('#energyByIndex').change(function() {
+define(['jquery'], function ($) {
     'use strict';
-    getEnergyByIndex();
-});
+    var index = 0 ;
+    console.log('renewables.js');
+    function getRenewable() {
+        console.log('getRenewable called');
 
-define(function() {
-    'use strict';
-
-    function getEnergyByIndex() {
-        var indexInput = $('#energyByIndex').val();
-        console.log('getEnergyByIndex called', indexInput);
-
-        $.getJSON('/high-energy-tech/byIndex/' + indexInput, function(response) {
+        $.getJSON('/high-tech-energy/ByIndex/' + index , function(response) {
                 console.log(response);
+
+                //ZZZZ.renewablesList = response.renewables; //  cc < ==== HERE
+                //showRenewable(ZZZZ.renewablesList[index]); //  cc < ==== HERE
+                showRenewable(response.renewables);
+
                 $('#debug').html(JSON.stringify(response, null, 4));
             })
-            .done(function() {
-                console.log('second success');
-            })
-            .fail(function(a, b, c) {
+            .fail(function (a, b, c) {
                 console.log('Error', a, b, c);
                 $('#debug').html('Error occured: ', a.status);
             })
-            .always(function() {
+            .done(function () {
+                console.log('second success');
+            })
+            .always(function () {
                 console.log('complete');
             });
     }
 
-    function indexButtonChange(event) {
-        var index = $('#energyByIndex').val();
-        $('#energyByIndex').val(parseInt(index) + event.data.value);
-        getEnergyByIndex();
+    function getSimpleKeys(renewable) {
+        'use strict';
+        return {
+            year: renewable['MSN'],
+            solar: renewable['YYYYMM'],
+            geo: renewable['Value'],
+            biomass: renewable['Column_Order'],
+            wind: renewable['Description'],
+            liquid: renewable['Unit'],
+        }
     }
 
-    var energyTypes = {
-        color: 'display of the energy data',
-        size: 'big',
-        init: function() {
-            console.log(energyTypes.color);
-            $('#elf-view').load('/high-energy-tech/renewables-by-index-page', function() { //r
-                $('#display').html(energyTypes.color);
-                $('#display2').html(energyTypes.size);
+    function showRenewable(renewable) {
+        'use strict';
+        var renewablex = getSimpleKeys(renewable);  // the variable name is now distinguished by a different name
+        $('#yearView').html(renewablex.year);    //.html is used instead of .val for the display
+        $('#solarView').html(renewablex.solar);
+        $('#geoView').html(renewablex.geo);
+        $('#biomassView').html(renewablex.biomass);
+        $('#windView').html(renewablex.wind);
+        $('#liquidView').html(renewablex.liquid);
+    }
 
-                $('#plusButton').click({
-                    value: 1
-                }, indexButtonChange);
-                $('#minusButton').click({
-                    value: -1
-                }, indexButtonChange);
-
-                //$('#energyByIndex').change(function() {
-                    getEnergyByIndex();
-                });
-                //getRenewableByIndex();
-            //});
+    function indexChange(test) {                 //   static form value is read and written back validated
+        if (test < 7009 && test >= 0) {
+            index = test;
+            $('#indexInput').val(index);         // the value is send back to the field
+            // showRenewable(ZZZZ.renewablesList[index]);
+            getRenewable();
         }
+    }
+
+    var indexButtonChange = function (event) {  // the user  +  or  - event is added or substracted to the value
+        var test = event.data.value + index;
+        indexChange(test);
     };
-    return energyTypes;
+
+    var buttonChange = function () {
+        var test = $('#indexInput').val();     // the default value or the user filled input is taken
+        indexChange(parseInt(test));
+    };
+
+    var ZZZZ = {                    // ppt < ==== needed a variable name that more distinctable than "renewables"
+        color: 'display of the energy data',
+        size: 'server side indexed (GET /:id )',
+        renewablesList: [],         //  cc < ==== HERE
+        getRenewable: getRenewable, //  cc < ==== HERE
+        init: function () {
+            console.log(ZZZZ.color);
+            $('#elf-view').load('/high-tech-energy/energy-types-page', function () {
+                $('#display').html(ZZZZ.color);
+                $('#display2').html(ZZZZ.size);
+                $('#plusButton').click({value: +1}, indexButtonChange);  // note the list has a descending order
+                $('#minusButton').click({value: -1}, indexButtonChange);
+                $('#indexInput').change(buttonChange);
+                //$('#XXXX').change(function () {     // ppt < ==  renamed for clarity purpose "renewables"
+                //    getRenewable();
+                //});                  // ppt < ====== seemed not beeing used
+                getRenewable();
+            });
+        }
+
+    };
+    return ZZZZ;
 });
