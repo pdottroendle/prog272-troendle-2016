f00a60ee7276f439805d72ac805854465cccb15d
Troendle <pdottroendle@gmail.com>
2016-06-09 21:15:20 -0700

    wk 10

--- Week10-MongooseBasics/routes/connect.js
+++ Week10-MongooseBasics/routes/connect.js
@@ -36,7 +36,7 @@ var connect = {
     },
 
     doConnection: function(useSimple) {
-        var connectType = useSimple || false;     // week 10
+        var connectType = useSimple || false;  // week 10
         if (connectType) {
             connect.simpleConnect();
         } else {
